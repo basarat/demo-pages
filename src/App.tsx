bd17a442b185ecc86d3d1825f2e3e116517934ce
Basarat <basaratali@gmail.com>
2023-01-24 17:33:57 +1100

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const inc = () => setCount(count => count + 1);
+  const dec = () => setCount(count => count - 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={dec}>-</button>
+      <div>{count}</div>
+      <button onClick={inc}>+</button>
+    </>
   );
 }
-
-export default App;
